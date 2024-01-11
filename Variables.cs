4d31615efd0c91910f97469d0203a024d63299f6
vaibhav19981 <149162142+vaibhav19981@users.noreply.github.com>
2024-01-11 17:47:16 +0530

    Update Variables.cs

---
+++ Variables.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Add
+{
+    internal class Variable{
+        public static void Main(string[] args){
+            int a=35;
+            float b=12.5;
+            char c = 'c';
+            string d = 'vaibhav';
+        }
+    }
+}
